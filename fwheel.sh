99b774e0cd136e404af9e91065f989832b3fd77b
Brett Longworth <blongworth@gmail.com>
2016-03-07 17:27:36 -0500

    new script to go from wheeler xls to txt wheelfile

---
+++ fwheel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Format raw wheeler output for USAMS
+# convert xlsx wheelfiles to tsv using csvkit
+# remove first line if neccesary
+# make all warmups 10s and make first standard 11 runs with awk
+
+infile="$1"
+outfile="${infile%.*}.txt"
+
+if [ -e "$outfile" ]
+then
+	echo "$outfile exists!"
+	exit
+fi
+
+# convert to tsv
+/usr/local/bin/in2csv $infile | /usr/local/bin/csvformat -T | \
+awk '{if (NR==1 && /^\*/) next};1' | \
+awk -F"\t" 'NR!=1 {$7="10";}1' OFS="\t" | \
+awk -F"\t" '/OX-I/ && !x {x=($8="11");}1' OFS="\t" > $outfile
