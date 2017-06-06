0c4a4416b324d2f7953d8d1dfa08e9e1113bc7cf
Brett Longworth <blongworth@gmail.com>
2017-06-06 11:09:33 -0400

    fix issue with long sample names

--- pwheel.sh
+++ pwheel.sh
@@ -2,6 +2,6 @@
 
 # format and print out wheelfile names on STDIN
 
-awk -F "\t" 'BEGIN { OFS=FS }; { $2=substr($2, 1, 25); print }' $1 | \
+awk -F "\t" 'BEGIN { OFS=FS }; { $2=substr($2, 1, 24); print }' $1 | \
 	csvlook -t |\
 	pr --length=63 --header=$1 | lpr -ocpi=13
